b8485dde71971f50365cff3d7e6b3941fc89ba5d
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-06 05:25:50 +0300

    Application entry point aka server

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+const port = process.env.PORT;
+
+app.listen(port, ()=> {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
