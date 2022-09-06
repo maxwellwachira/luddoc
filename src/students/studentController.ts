6b2ebd5d1bf1b0578cd11bfd4910c3e2f6926f4e
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-06 05:32:24 +0300

    student module

---
+++ src/students/studentController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+const studentProfile = async (req: Request, res: Response) => {
+
+}
+
+const editProfile =  async (req: Request, res: Response) => {
+
+}
+
+const deleteProfile = async (req: Request, res: Response) => {
+
+}
+
+
+export {
+    deleteProfile,
+    editProfile,
+    studentProfile
+}
