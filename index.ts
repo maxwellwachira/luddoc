d590b371f69cfad86fe3ee52b50e789bd525418d
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-07 05:35:08 +0300

    aded auth and user routes

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 
+//Sequelize Database Connector
+import { dbAuthenticate } from "./src/config/dbconfig";
+//Authentication Routes
+import authRoutes from "./src/authentication/emailAuth/authRoutes";
+//User Routes
+import userRoutes from "./src/users/userRoutes";
+
 dotenv.config();
 
 const app: Application = express();
-
 const port = process.env.PORT;
+
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 app.listen(port, ()=> {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    dbAuthenticate();
 });
