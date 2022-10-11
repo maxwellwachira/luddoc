f5b7593f7529a7d0b5a1b2dd7bebf7eded341253
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-11 13:54:19 +0300

    updates to cors

--- index.ts
+++ index.ts
@@ -17,9 +17,11 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT;
 
+app.use(cors({}));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
