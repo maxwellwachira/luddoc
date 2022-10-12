5ea58a9cec9db71a15b90cf3527c909275fc3f67
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-12 09:37:38 +0300

    changed google smtp from password to oauth2

--- src/email/sendEmail.ts
+++ src/email/sendEmail.ts
@@ -1,10 +1,13 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const user = process.env.NODEMAILER_USER;
-const pass = process.env.NODEMAILER_PASS;
+const user = process.env.NODEMAILER_USER as string;
+const clientId = process.env.GOOGLE_CLIENT_ID as string;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET as string;
+const refreshToken = process.env.GOOGLE_REFRESH_TOKEN as string;
 
 interface SendMailData {
     to: string;
@@ -13,15 +16,45 @@ interface SendMailData {
     html?: string;
 }
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user,
-        pass
-    }
-});
+const OAuth2 = google.auth.OAuth2;
+
+
+const createTransporter = async () => {
+
+    const oauth2Client = new OAuth2(
+        clientId,
+        clientSecret,
+        "https://developers.google.com/oauthplayground"
+    );
+    
+    oauth2Client.setCredentials({
+        refresh_token: refreshToken
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            reject("Failed to create access token :(");
+          }
+          resolve(token);
+        });
+      });
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: "OAuth2",
+            user,
+            accessToken: accessToken as string,
+            clientId,
+            clientSecret,
+            refreshToken
+        }
+    });
+    
+    return transporter;
+}
+
 
 export const sendMail =  async ({ to, subject, text, html }: SendMailData) => {
     if(!text) text = '';
@@ -35,7 +68,8 @@ export const sendMail =  async ({ to, subject, text, html }: SendMailData) => {
     };
 
     try {
-        const send = await transporter.sendMail(mailOptions);
+        let emailTransporter = await createTransporter();
+        const send = await emailTransporter.sendMail(mailOptions);
         console.log(send.response);
     } catch (error) {
         console.log(error);
