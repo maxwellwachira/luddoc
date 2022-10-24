87d5426c1443df3f7d37a2974f67793e853b585e
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-24 10:49:28 +0300

    added image send file route

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from "cors";
+import fileUpload from 'express-fileupload';
+import path from "path";
 import dotenv from 'dotenv';
 
 //Sequelize Database Connector
@@ -7,11 +9,16 @@ import { dbAuthenticate } from "./src/config/dbconfig";
 //Authentication Routes
 import authRoutes from "./src/authentication/emailAuth/authRoutes";
 //User Routes
-import userRoutes from "./src/users/userRoutes";
+import userRoutes from "./src/appUsers/users/userRoutes";
 //zoom routes
 import ZoomRoutes from "./src/zoom/zoomRoutes";
 //email routes
 import emailRoutes from "./src/email/emailRoutes";
+//category routes
+import categoryRoutes from "./src/lms/categories/categoryRoutes";
+//course routes
+import courseRoutes from "./src/lms/courses/courseRoutes";
+
 dotenv.config();
 
 const app: Application = express();
@@ -19,16 +26,27 @@ const port = process.env.PORT;
 
 app.use(cors({}));
 app.options('*', cors());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({extended: false}));
+app.use(fileUpload({
+    createParentPath: true
+}));
 
+app.get('/image', (req: Request, res: Response) => {
+    const filePath = req.query.filePath as string;
+    const fullPath = path.join(__dirname, `../${filePath}`);
+    console.log(fullPath);
+    res.sendFile(fullPath);
+})
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/zoom', ZoomRoutes);
-app.use('/email',emailRoutes);
+app.use('/email',emailRoutes);    
+app.use('/category', categoryRoutes);
+app.use('/course', courseRoutes);
 
-app.listen(port, ()=> {
+app.listen(port,() => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
     dbAuthenticate();
 });
