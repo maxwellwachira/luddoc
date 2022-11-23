b11060b4dd79cc07bdcf2577a5022a9276ea4271
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-23 20:02:56 +0300

    changed email provider from nodemail to mailjet

--- src/email/emailController.ts
+++ src/email/emailController.ts
@@ -6,7 +6,7 @@ import emailTemplates  from "./emailTemplates"
 const sendContactForm =async  (req: Request, res: Response) => {
     const { email, subject, message } = req.body;
     try {
-        await sendMail({to: 'maxwellwachira67@gmail.com', subject: 'Web Contact Form', html: emailTemplates.contactForm(email, subject, message)});
+        await sendMail({to: 'info@luddoc-institute.com', subject: 'Web Contact Form', html: emailTemplates.contactForm(email, subject, message)});
         res.status(200).json({message: "success"});
     } catch (error) {
         console.log(error);
--- src/email/sendEmail.ts
+++ src/email/sendEmail.ts
@@ -1,76 +1,47 @@
-import nodemailer from "nodemailer";
-import { google } from "googleapis";
+import Mailjet  from "node-mailjet";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const user = process.env.NODEMAILER_USER as string;
-const clientId = process.env.GOOGLE_CLIENT_ID as string;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET as string;
-const refreshToken = process.env.GOOGLE_REFRESH_TOKEN as string;
 
 interface SendMailData {
     to: string;
     subject: string;
     text?: string;
     html?: string;
-}
+};
 
-const OAuth2 = google.auth.OAuth2;
+const apiKey = process.env.MAILJET_API_KEY as string;
+const apiSecret = process.env.MAILJET_API_SECRET as string;
 
-
-const createTransporter = async () => {
-
-    const oauth2Client = new OAuth2(
-        clientId,
-        clientSecret,
-        "https://developers.google.com/oauthplayground"
-    );
-    
-    oauth2Client.setCredentials({
-        refresh_token: refreshToken
-    });
-
-    const accessToken = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err, token) => {
-          if (err) {
-            reject("Failed to create access token :(");
-          }
-          resolve(token);
-        });
-      });
-
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: "OAuth2",
-            user,
-            accessToken: accessToken as string,
-            clientId,
-            clientSecret,
-            refreshToken
-        }
-    });
-    
-    return transporter;
-}
-
-
+const mailjet =Mailjet.apiConnect(
+   apiKey,
+   apiSecret
+);
+     
 export const sendMail =  async ({ to, subject, text, html }: SendMailData) => {
-    if(!text) text = '';
-
-    const mailOptions = {
-        from: user,
-        to,
-        subject,
-        text,
-        html
-    };
-
     try {
-        let emailTransporter = await createTransporter();
-        const send = await emailTransporter.sendMail(mailOptions);
-        console.log(send.response);
+        const request = await mailjet
+        .post("send", {'version': 'v3.1'})
+        .request({
+            "Messages":[
+                    {
+                            "From": {
+                                    "Email": "info@luddoc-institute.com",
+                                    "Name": "Luddoc Institute"
+                            },
+                            "To": [
+                                    {
+                                            "Email": `${to}`,
+                                            "Name":`${to}`
+                                    }
+                            ],
+                            "Subject": `${subject}`,
+                            "TextPart": `${text}`,
+                            "HTMLPart": `${html}`
+                    }
+            ]
+        });
+        console.log(request);
     } catch (error) {
         console.log(error);
     }
